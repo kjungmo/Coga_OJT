9c70a24ac3dae177d199bc7a8a8479bfdf87c7b9
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-12-22 16:58:53 +0900

    .txt 파일을 읽고, 파일 내 숫자를 프롬프트에 출력

--- matmul/matmul/main.c
+++ matmul/matmul/main.c
@@ -1,17 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+struct matrix {
+	int rows;
+	int columns;
+	int m11;
+	int m12;
+	int m21;
+	int m22;
+};
+
 int main()
 {
-	FILE* fp = fopen("AA.txt", "r");
-	char string[255];
-	char string2[255];
-	int number;
+	struct matrix m;
+	FILE *fp;
+	fp = fopen("B.txt", "r");
+	//char string[255];
+	//char string2[255];
 
 	//fprintf(fp, "hello world! %d", 1234);
 
-	fscanf(fp, "%s%s%d", string, string2, &number);
-	printf("%s %s %d\n", string, string2, number);
+	fscanf(fp, "%d %d %d %d %d %d", &m.rows, &m.columns, &m.m11, &m.m12, &m.m21, &m.m22);
+	printf("B = \n%d %d \n%d %d", m.m11, m.m12, m.m21, m.m22);
 
 	fclose(fp);
 
