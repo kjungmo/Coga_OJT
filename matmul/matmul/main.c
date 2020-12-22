9ad5be021afe4ecf3839be6e6e1f7ebda82652e3
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-12-22 23:41:15 +0900

    argc, argv 사용하여 파일명 입력 및 콘솔창 출력

--- matmul/matmul/main.c
+++ matmul/matmul/main.c
@@ -10,30 +10,50 @@ struct matrix {
 	int m22;
 };
 
-int main()
+int main(int argc, char *argv[])
 {
-	struct matrix ma;
-	FILE *fp_a;
-	fp_a = fopen("A.txt", "r");
-	fscanf(fp_a, "%d %d %d %d %d %d", &ma.rows, &ma.columns, &ma.m11, &ma.m12, &ma.m21, &ma.m22);
-	printf("A = \n%d %d \n%d %d", ma.m11, ma.m12, ma.m21, ma.m22);
-	fclose(fp_a);
-	
-	printf("\n");
-	
-	struct matrix mb;
-	FILE *fp_b;
-	fp_b = fopen("B.txt", "r");
-	fscanf(fp_b, "%d %d %d %d %d %d", &mb.rows, &mb.columns, &mb.m11, &mb.m12, &mb.m21, &mb.m22);
-	printf("B = \n%d %d \n%d %d", mb.m11, mb.m12, mb.m21, mb.m22);
-	fclose(fp_b);
 
-	printf("\n");
+	/*struct matrix m;
+	FILE *fp;*/
+	//printf("%s", argv[1]);
+	for (int i = 0; i < argc; i++)
+	{
+		//char *fname = argv[i];
+		//fp = fopen(*fname, "r");
+		printf("argv[%d] = %s\n", i, argv[i]);
+		/*if ((fp = fopen(*fname, "r")) == NULL)
+		{
+			fprintf(stderr, "파일 %s을 열 수 없습니다.\n", *fname);
+			exit(1);
+		}*/
+		/*fscanf(fp, "%d %d %d %d %d %d", &m.rows, &m.columns, &m.m11, &m.m12, &m.m21, &m.m22);
+		printf("A = \n%d %d \n%d %d\n", m.m11, m.m12, m.m21, m.m22);*/
 
-	printf("AB = \n%d %d \n%d %d ", 
-		ma.m11*mb.m11+ma.m12*mb.m21, ma.m11*mb.m12+ma.m12*mb.m22,
-		ma.m21*mb.m11+ma.m22*mb.m21, ma.m21*mb.m12+ma.m22*mb.m22
-		);
+		//fclose(fp);
+	}
+
+	//struct matrix ma;
+	//FILE *fp_a;
+	//fp_a = fopen("A.txt", "r");
+	//fscanf(fp_a, "%d %d %d %d %d %d", &ma.rows, &ma.columns, &ma.m11, &ma.m12, &ma.m21, &ma.m22);
+	//printf("A = \n%d %d \n%d %d", ma.m11, ma.m12, ma.m21, ma.m22);
+	//fclose(fp_a);
+	//
+	//printf("\n");
+	//
+	//struct matrix mb;
+	//FILE *fp_b;
+	//fp_b = fopen("B.txt", "r");
+	//fscanf(fp_b, "%d %d %d %d %d %d", &mb.rows, &mb.columns, &mb.m11, &mb.m12, &mb.m21, &mb.m22);
+	//printf("B = \n%d %d \n%d %d", mb.m11, mb.m12, mb.m21, mb.m22);
+	//fclose(fp_b);
+
+	//printf("\n");
+
+	//printf("AB = \n%d %d \n%d %d ", 
+	//	ma.m11*mb.m11+ma.m12*mb.m21, ma.m11*mb.m12+ma.m12*mb.m22,
+	//	ma.m21*mb.m11+ma.m22*mb.m21, ma.m21*mb.m12+ma.m22*mb.m22
+	//	);
 
 	return 0;
 }
