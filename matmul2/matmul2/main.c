1202559a1e0a6c98b8a1f1d04c4278e33cee70cb
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-12-24 17:16:52 +0900

    argv[] matrix values print automation

--- matmul2/matmul2/main.c
+++ matmul2/matmul2/main.c
@@ -10,13 +10,38 @@
 int main(int argc, char *argv[])
 {
 
-	int a, b, c, d, e, f;
+	int row, col, i, j, k, l;
+
+	// 명령행 인자로 들어오는 파일들로부터 함수 값 출력
 	for (int i = 1; i < argc; i++)
 	{
 		FILE *f = fopen(argv[i], "r");
-		fscanf(f, "%d%d", &a, &b);
-		printf("%d %d\n", a, b);
+		fscanf(f, "%d%d", &row, &col);
+		//printf("%d %d\n", row, col);
 
+		// 행렬 메모리 할당
+		int **mat = (int**)malloc((sizeof(int*)) * row);
+		for (j = 0; j < row; j++)
+		{
+			*(mat + j) = (int*)malloc(sizeof(int) * col);
+		}
+ 
+		char *sliced = strtok(argv[i], ".");
+		printf("%s = \n", sliced);
+
+		//행렬 값 넣어서 출력
+		for (k = 0; k < row; k++)
+		{
+			for (l = 0; l < col; l++)
+			{
+				fscanf(f, "%d", *(mat + k) + l);
+				printf("%d ", *(*(mat + k) + l));
+			}
+			printf("\n");
+		}
+		printf("\n");
+		fclose(f);
+		
 	}
 
 
