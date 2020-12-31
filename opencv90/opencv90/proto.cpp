0eedf40ee5fc7ce991b21c9afd3f5b072ce63487
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-12-31 14:15:22 +0900

    using Mat::at to get pixel values and rotate the coordinates in loop

--- opencv90/opencv90/proto.cpp
+++ opencv90/opencv90/proto.cpp
@@ -1,11 +1,12 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
 
 
-int main()
+int maiwn()
 {
     cout << "Hello OpenCV " << CV_VERSION << endl;
 
@@ -16,8 +17,26 @@ int main()
         cerr << "Image load failed" << endl;
         return -1;
     }
-    Mat imgROI = img(Rect(200, 250, 200, 250));
-    imgROI = ~imgROI;
+
+
+    Mat imgROI1 = img(Rect(200, 250, 200, 250));
+    imgROI1 = ~imgROI1;
+    Mat imgROI2 = img(Rect(200, 250, 200, 250)).clone();
+    //Error
+    //Mat imgROI2 = img(Rect(200, 320, 200, 320)).clone(); 
+    imgROI2 = imgROI2;
+
+    Mat rotated90(Size(512, 512), CV_8UC1);
+
+    for (int j = 0; j < img.rows; j++)
+    {
+        for (int i = 0; i < img.cols; i++)
+        {
+            rotated90.at<uchar>(i, img.rows - 1 - j) = img.at<uchar>(j, i);
+            int val = img.at<uchar>(j, i);  
+            //printf("%d ", val);
+        }
+    }
 
     // diffenence between just 0 and Scalar(0)?
     //Mat practice1(300, 300, CV_8UC1, 0);
@@ -29,10 +48,13 @@ int main()
     img = ~img;
     //namedWindow("image");
     imshow("image", img);
-    
+     
     // without using namedWindow, still works fine
     //namedWindow("imageROI");
-    imshow("imageROI", imgROI);
+    imshow("imageROI2", imgROI1);
+    imshow("imageROI1", imgROI2);
+    
+    imshow("90RO", rotated90);
 
     //namedWindow("practice image");
     imshow("practice image", practice1);
