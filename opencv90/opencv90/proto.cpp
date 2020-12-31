e7b91e46b646712292a815bf698bbcdd3063f503
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-12-31 13:07:23 +0900

    Mat class declared, practicing with Mat members

--- opencv90/opencv90/proto.cpp
+++ opencv90/opencv90/proto.cpp
@@ -4,20 +4,39 @@
 using namespace cv;
 using namespace std;
 
+
 int main()
 {
     cout << "Hello OpenCV " << CV_VERSION << endl;
 
-    Mat img;
-    img = imread("a.jpg");
+    Mat img; // empty Mat matrix declared 
+    img = imread("a.jpg", IMREAD_GRAYSCALE);
 
     if (img.empty()) {
         cerr << "Image load failed" << endl;
         return -1;
     }
+    Mat imgROI = img(Rect(200, 250, 200, 250));
+    imgROI = ~imgROI;
 
-    namedWindow("image");
+    // diffenence between just 0 and Scalar(0)?
+    //Mat practice1(300, 300, CV_8UC1, 0);
+     Mat practice1(300, 300, CV_8UC1, Scalar(0));
+
+    Mat practice2 = practice1.clone();
+    //cout << img.row << endl;
+
+    img = ~img;
+    //namedWindow("image");
     imshow("image", img);
+    
+    // without using namedWindow, still works fine
+    //namedWindow("imageROI");
+    imshow("imageROI", imgROI);
+
+    //namedWindow("practice image");
+    imshow("practice image", practice1);
+
 
     waitKey();
     return 0;
