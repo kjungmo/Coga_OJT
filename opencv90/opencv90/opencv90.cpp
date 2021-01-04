b07751121229bce1b08ec6254988c254409aba58
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-04 14:13:31 +0900

    function name changed(forward)

--- opencv90/opencv90/opencv90.cpp
+++ opencv90/opencv90/opencv90.cpp
@@ -7,7 +7,7 @@
 using namespace cv;
 using namespace std;
 
-Mat getRotatedImage(Mat image, int degrees);
+Mat forward(Mat image, int degree);
 
 int main(int argc, char *argv[])
 {
@@ -15,47 +15,48 @@ int main(int argc, char *argv[])
     Mat sourceImage = imread(argv[1], IMREAD_GRAYSCALE); // check release or debug
    
    // get degrees
-   int degrees = atoi(argv[2]);
+   int degree = atoi(argv[2]);
+   printf("Image has been %d degrees rotated", degree);
+   printf("\n");
 
    // get rotated Image
-   Mat rotatedImage = getRotatedImage(sourceImage, degrees);
+   Mat rotated = forward(sourceImage, degree);
 
    imshow("source Image", sourceImage);
-   imshow("rotated Image", rotatedImage);
+   imshow("rotated Image", rotated);
    
     waitKey(0);
     return 0;
 } 
 
-Mat getRotatedImage(Mat image, int degrees)
+Mat forward(Mat image, int degree)
 {
     int W, H;
     W = image.cols;
     H = image.rows;
     int centerX = W / 2;
     int centerY = H / 2;
-    //int theta = degrees * (PI / 180); // 180 anticlockwise // 40 clockwise(one function) 
-    //int theta = (PI / 180) / degrees; // 90 clockwise
-    int theta = degrees / (PI / 180); // holes, 30 anticlockwise
+
     
-    Mat targetImage(Size(image.rows, image.cols), CV_8UC1);
+    double theta = degree * (PI / 180);
+
+    Mat targetImage(Size(W, H), CV_8UC1);
 
 
-    for (int i = 0; i < targetImage.rows; i++)
+    for (int x = 0; x < targetImage.cols; x++)
     {
-        for (int j = 0; j < targetImage.cols; j++)
+        for (int y = 0; y < targetImage.rows; y++)
         {
             
-            //targetImage.at<uchar>(i, j) = image.at<uchar>(getNewX(i, j, degrees), getNewY(i, j, degrees));
-            int newX = (int)(cos(theta) * (i - centerX) + sin(theta) * (j - centerY) + centerX);
-            int newY = (int)(-sin(theta) * (i - centerX) + cos(theta) * (j - centerY) + centerY);
+            int newX = (int)(cos(theta) * (x - centerX) + sin(theta) * (y - centerY) + centerX);
+            int newY = (int)(-sin(theta) * (x - centerX) + cos(theta) * (y - centerY) + centerY);
             
             if(newX < 0)            continue;
             if(newX >= image.cols)  continue;
             if(newY < 0)            continue;
             if(newY >= image.rows)  continue;
             
-            targetImage.at<uchar>(newX,newY) = image.at<uchar>(i, j);
+            targetImage.at<uchar>(newY, newX) = image.at<uchar>(y, x);
         }
     }
 
