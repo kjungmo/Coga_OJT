feeb82db1f7addd4f7004a28d2aa77457b06d988
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-04 10:55:23 +0900

    created getRotatedMatrix to use rotation Matrix values

--- opencv90/opencv90/opencv90.cpp
+++ opencv90/opencv90/opencv90.cpp
@@ -1,49 +1,66 @@
-#include <opencv2/opencv.hpp>
-#include <math.h>
+#include "opencv2/opencv.hpp"
 #include <iostream>
+#include <math.h>
 
+#define PI 3.1415926535897
 
 using namespace cv;
 using namespace std;
 
+Mat getRotatedMatrix(Mat image, int degrees);
+double getRadian(int _num);
+int getNewX(int x, int y, int degrees);
+int getNewY(int x, int y, int degrees);
 
-int mai-n(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     // read image
-    Mat img = imread(argv[1], IMREAD_GRAYSCALE); // check release or debug
+    Mat sourceImage = imread(argv[1], IMREAD_GRAYSCALE); // check release or debug
    
+   // get degrees
+   int degrees = atoi(argv[2]);
 
-   //rotate 90 degrees clockwise
-    Mat rotateClockwise90(Size(img.cols, img.rows), CV_8UC1);
-    Mat rotateAntiCwise90(Size(img.cols, img.rows), CV_8UC1);
+   // get rotated Image
+   Mat rotatedImage = getRotatedMatrix(sourceImage, degrees);
 
-
-    // clockwise 90 degrees
-    for (int j = 0; j < img.rows; j++)
-    {
-        for (int i = 0; i < img.cols; i++)
-        {
-            rotateClockwise90.at<uchar>(i, img.rows - 1 - j) = img.at<uchar>(j, i);
-            
-        }
-    }
-    
-
-    // anti-clockwise 90 degrees ( clockwise 270 degrees)
-    for (int j = 0; j < img.rows; j++)
-    {
-        for (int i = 0; i < img.cols; i++)
-        {
-            rotateAntiCwise90.at<uchar>(img.rows - 1 - i, j) = img.at<uchar>(j, i);
-
-        }
-    }
-
-
-    imshow("image", img);
-    imshow("clockwise 90", rotateClockwise90);
-    imshow("anti-clockwise 90", rotateAntiCwise90);
-
+   imshow("source Image", sourceImage);
+   imshow("rotated Image", rotatedImage);
+   
     waitKey(0);
     return 0;
 } 
+
+Mat getRotatedMatrix(Mat image, int degrees)
+{
+
+    
+    Mat targetImage(Size(image.rows, image.cols), CV_8UC1);
+
+
+    for (int i = 0; i < targetImage.rows; i++)
+    {
+        for (int j = 0; j < targetImage.cols; j++)
+        {
+            targetImage.at<uchar>(i, j) = image.at<uchar>(getNewX(i, j, degrees), getNewY(i, j, degrees));
+        }
+    }
+
+    return targetImage;
+}
+
+double getRadian(int _num)
+{
+    return _num * (PI / 180);
+}
+
+int getNewX(int x, int y, int degrees)
+{
+    int newX = (int)( (cos(getRadian(degrees)) * x) - (sin(getRadian(degrees)) * y) );
+    return newX;
+}
+
+int getNewY(int x, int y, int degrees)
+{
+    int newY = (int)( (sin(getRadian(degrees)) * x) + (cos(getRadian(degrees)) * y) );
+    return newY;
+}
--- opencv90/opencv90/proto.cpp
+++ opencv90/opencv90/proto.cpp
@@ -1,91 +1,129 @@
-#include "opencv2/opencv.hpp"
-#include <iostream>
-#include <math.h>
-
-#define PI 3.1415926535897
-
-using namespace cv;
-using namespace std;
-
-class RotationMatrix
-{
-private:
-
-public:
-
-
-};
-
-double getRadian(int _num)
-{
-    return _num * (PI / 180);
-}
-
-double getRotatedMatrix(char *image, int degrees, float scale)
-{
-    int x, y, degrees;
-    double newX, newY;
-    sin = sin(getRadian(degrees));
-    cos = cos(getRadian(degrees));
-    newX = (cos * degrees * x) - (sin * degrees * y) // + centerX
-    newY = -(sin * degrees * x) + (cos * degrees * y) // + centerY
-}
-
-int main()
-{
-    cout << "Hello OpenCV " << CV_VERSION << endl;
-
-    Mat img; // empty Mat matrix declared 
-    img = imread("a.jpg", IMREAD_GRAYSCALE);
-
-    if (img.empty()) {
-        cerr << "Image load failed" << endl;
-        return -1;
-    }
-
-
-    Mat imgROI1 = img(Rect(200, 250, 200, 250));
-    imgROI1 = ~imgROI1;
-    Mat imgROI2 = img(Rect(200, 250, 200, 250)).clone();
-    //Error ( because Rect( int _x, int _y, int _width, int _height) not Rect(int _rowrange1, int _rowrange2, int _colrange1, int _colrange2)) 
-    //Mat imgROI2 = img(Rect(200, 320, 200, 320)).clone(); 
-    imgROI2 = imgROI2;
-
-    Mat rotated90(Size(512, 512), CV_8UC1);
-
-    for (int j = 0; j < img.rows; j++)
-    {
-        for (int i = 0; i < img.cols; i++)
-        {
-            rotated90.at<uchar>(i, img.rows - 1 - j) = img.at<uchar>(j, i);
-            int val = img.at<uchar>(j, i);  
-            cout << val << endl;//printf("%d ", val);
-        }
-    }
-
-    // diffenence between just 0 and Scalar(0)?
-    // MUST use Scalar() / not int ( see document [ void* ])
-    Mat practice1(300, 300, CV_8UC1, 1);
-     //Mat practice1(300, 300, CV_8UC1, Scalar(0));
-
-    Mat practice2 = practice1.clone();
-    //cout << img.row << endl;
-
-    img = ~img;
-    //namedWindow("image");
-    imshow("image", img);
-     
-    // without using namedWindow, still works fine
-    //namedWindow("imageROI");
-    imshow("imageROI2", imgROI1);
-    imshow("imageROI1", imgROI2);
-    
-    imshow("90RO", rotated90);
-
-    //namedWindow("practice image");
-    imshow("practice image", practice1);
-
-
-    waitKey();
-    return 0;
-}
+//#include "opencv2/opencv.hpp"
+//#include <iostream>
+//#include <math.h>
+//
+//#define PI 3.1415926535897
+//
+//using namespace cv;
+//using namespace std;
+//
+//class RotationMatrix
+//{
+//private:
+//
+//public:
+//
+//
+//};
+//
+//double getRadian(int _num)
+//{
+//    return _num * (PI / 180);
+//}
+//
+//double getRotatedMatrix(char *image, int degrees)
+//{
+//    //int x, y;
+//    //double newX, newY, sinSeta, cosSeta;
+//
+//    Mat sourceImage = imread(image, IMREAD_GRAYSCALE);
+//    Mat targetImage(Size(sourceImage.rows, sourceImage.cols), CV_8UC1);
+//
+//    //sinSeta = sin(getRadian(degrees));
+//    //cosSeta = cos(getRadian(degrees));
+//    //// rotation of coordinates
+//    //newX = (cosSeta * x) - (sinSeta * y); // + centerX
+//    //newY = (sinSeta * x) + (cosSeta * y); // + centerY
+//    for (int i = 0; i < targetImage.rows; i++)
+//    {
+//        for (int j = 0; j < targetImage.cols; j++)
+//        {
+//            targetImage.at<uchar>(i, j) = sourceImage.at<uchar>(getNewX(i, j, degrees), getNewY(i, j, degrees));
+//        }
+//    }
+//    // rotation od axes
+//    //newX = (cosSeta * x) + (sinSeta * y) // + centerX
+//    //newY = -(sinSeta * x) + (cosSeta * y) // + centerY
+//    return 0;
+//}
+//
+//int getNewX(int x, int y, int degrees)
+//{
+//    int newX = (cos(getRadian(degrees)) * x) - (sin(getRadian(degrees)) * y);
+//    return newX;
+//}
+//
+//int getNewY(int x, int y, int degrees)
+//{
+//    int newY = (sin(getRadian(degrees)) * x) + (cos(getRadian(degrees)) * y);
+//    return newY;
+//}
+//
+//void mapForward()
+//{
+//    
+//}
+//
+//void mapBackward()
+//{
+//
+//}
+//
+//int mains()
+//{
+//    cout << "Hello OpenCV " << CV_VERSION << endl;
+//
+//    Mat sourceImage; // empty Mat matrix declared 
+//    sourceImage = imread("a.jpg", IMREAD_GRAYSCALE);
+//    cout << "rows : " << sourceImage.rows << endl;
+//    if (sourceImage.empty()) {
+//        cerr << "Image load failed" << endl;
+//        return -1;
+//    }
+//
+//
+//    Mat imgROI1 = sourceImage(Rect(200, 250, 200, 250));
+//    imgROI1 = ~imgROI1;
+//    Mat imgROI2 = sourceImage(Rect(200, 250, 200, 250)).clone();
+//    //Error ( because Rect( int _x, int _y, int _width, int _height) not Rect(int _rowrange1, int _rowrange2, int _colrange1, int _colrange2)) 
+//    //Mat imgROI2 = img(Rect(200, 320, 200, 320)).clone(); 
+//    imgROI2 = imgROI2;
+//
+//    Mat rotated90(Size(512, 512), CV_8UC1);
+//
+//    for (int j = 0; j < sourceImage.rows; j++)
+//    {
+//        for (int i = 0; i < sourceImage.cols; i++)
+//        {
+//            rotated90.at<uchar>(i, sourceImage.rows - 1 - j) = sourceImage.at<uchar>(j, i);
+//            int val = sourceImage.at<uchar>(j, i);
+//            //cout << val << endl;//printf("%d ", val);
+//        }
+//    }
+//
+//    // diffenence between just 0 and Scalar(0)?
+//    // MUST use Scalar() / not int ( see document [ void* ])
+//    Mat practice1(300, 300, CV_8UC1, 1);
+//     //Mat practice1(300, 300, CV_8UC1, Scalar(0));
+//
+//    Mat practice2 = practice1.clone();
+//    //cout << img.row << endl;
+//
+//    sourceImage = ~sourceImage;
+//    //namedWindow("image");
+//    imshow("image", sourceImage);
+//     
+//    // without using namedWindow, still works fine
+//    //namedWindow("imageROI");
+//    imshow("imageROI2", imgROI1);
+//    imshow("imageROI1", imgROI2);
+//    
+//    imshow("90RO", rotated90);
+//
+//    //namedWindow("practice image");
+//    imshow("practice image", practice1);
+//
+//
+//    waitKey();
+//    return 0;
+//}
