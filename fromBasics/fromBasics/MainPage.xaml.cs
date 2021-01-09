2161af2360e010917ec47fbc73672a8c281aeb22
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-10 00:04:37 +0900

    message printed by button click

--- fromBasics/fromBasics/MainPage.xaml.cs
+++ fromBasics/fromBasics/MainPage.xaml.cs
@@ -29,7 +29,13 @@ namespace fromBasics
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-
+            string name = "Quentin";
+            int x = 3;
+            x = x * 17;
+            double d = Math.PI / 2;
+            myLabel.Text = "name is " + name
+                + "\nx is " + x
+                + "\nd is " + d;
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
