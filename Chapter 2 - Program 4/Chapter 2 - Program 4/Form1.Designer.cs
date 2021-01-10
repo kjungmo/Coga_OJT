3f1f1ef86250c069417328e36e0227478c6dd0ab
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-10 20:35:55 +0900

    created windows app

---
+++ Chapter 2 - Program 4/Chapter 2 - Program 4/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Chapter_2___Program_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button changeText;
+            this.enableCheckbox = new System.Windows.Forms.CheckBox();
+            this.labelToChange = new System.Windows.Forms.Label();
+            changeText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // changeText
+            // 
+            changeText.AutoSize = true;
+            changeText.Location = new System.Drawing.Point(12, 12);
+            changeText.Name = "changeText";
+            changeText.Size = new System.Drawing.Size(173, 23);
+            changeText.TabIndex = 0;
+            changeText.Text = "Change the label if checked";
+            changeText.UseVisualStyleBackColor = true;
+            changeText.Click += new System.EventHandler(this.changeText_Click);
+            // 
+            // enableCheckbox
+            // 
+            this.enableCheckbox.AutoSize = true;
+            this.enableCheckbox.Checked = true;
+            this.enableCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableCheckbox.Location = new System.Drawing.Point(322, 16);
+            this.enableCheckbox.Name = "enableCheckbox";
+            this.enableCheckbox.Size = new System.Drawing.Size(150, 16);
+            this.enableCheckbox.TabIndex = 1;
+            this.enableCheckbox.Text = "Enable label changing";
+            this.enableCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // labelToChange
+            // 
+            this.labelToChange.Location = new System.Drawing.Point(2, 54);
+            this.labelToChange.Name = "labelToChange";
+            this.labelToChange.Size = new System.Drawing.Size(481, 23);
+            this.labelToChange.TabIndex = 2;
+            this.labelToChange.Text = "Press the button to change my text";
+            this.labelToChange.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 91);
+            this.Controls.Add(this.labelToChange);
+            this.Controls.Add(this.enableCheckbox);
+            this.Controls.Add(changeText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox enableCheckbox;
+        private System.Windows.Forms.Label labelToChange;
+    }
+}
+
