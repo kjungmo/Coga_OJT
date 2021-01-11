7a72f0c2d8ff4d1f1d15b6b67c17b89387d8a58d
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-11 19:10:45 +0900

    created Hit the keys! game

---
+++ HeadFirst_Csharp/Key Input Game/Key Input Game/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Key_Input_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.correctLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.missedLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.totalLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.accuracyLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.difficultyProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.correctLabel,
+            this.missedLabel,
+            this.totalLabel,
+            this.accuracyLabel,
+            this.toolStripStatusLabel1,
+            this.difficultyProgressBar});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.SizingGrip = false;
+            // 
+            // correctLabel
+            // 
+            this.correctLabel.Name = "correctLabel";
+            resources.ApplyResources(this.correctLabel, "correctLabel");
+            // 
+            // missedLabel
+            // 
+            this.missedLabel.Name = "missedLabel";
+            resources.ApplyResources(this.missedLabel, "missedLabel");
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Name = "totalLabel";
+            resources.ApplyResources(this.totalLabel, "totalLabel");
+            // 
+            // accuracyLabel
+            // 
+            this.accuracyLabel.Name = "accuracyLabel";
+            resources.ApplyResources(this.accuracyLabel, "accuracyLabel");
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
+            this.toolStripStatusLabel1.Spring = true;
+            // 
+            // difficultyProgressBar
+            // 
+            this.difficultyProgressBar.Maximum = 701;
+            this.difficultyProgressBar.Name = "difficultyProgressBar";
+            resources.ApplyResources(this.difficultyProgressBar, "difficultyProgressBar");
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel correctLabel;
+        private System.Windows.Forms.ToolStripStatusLabel missedLabel;
+        private System.Windows.Forms.ToolStripStatusLabel totalLabel;
+        private System.Windows.Forms.ToolStripStatusLabel accuracyLabel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar difficultyProgressBar;
+    }
+}
+
