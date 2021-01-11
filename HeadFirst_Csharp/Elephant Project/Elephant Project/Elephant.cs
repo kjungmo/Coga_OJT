04990dd204f487b030becd98635353ac7adb09ad
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-11 18:24:20 +0900

    more function added

---
+++ HeadFirst_Csharp/Elephant Project/Elephant Project/Elephant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elephant_Project
+{
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            MessageBox.Show("My ears are " + EarSize + " inches tall.", Name + " says...");
+        }
+
+        public void TellMe(string message, Elephant whoSaidIt)
+        {
+            MessageBox.Show(whoSaidIt.Name + " says: " + message);
+        }
+        
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.TellMe(message, this);
+        }
+    }
+}
