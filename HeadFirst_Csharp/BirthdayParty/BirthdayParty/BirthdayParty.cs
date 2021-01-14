df3316961c5721c9f9fe54394f3b481bc2bc52fe
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-14 13:57:21 +0900

    created Dinner Party and Birthday Party calculator

---
+++ HeadFirst_Csharp/BirthdayParty/BirthdayParty/BirthdayParty.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayParty
+{
+    class BirthdayParty
+    {
+        public const decimal costOfFoodPerPerson = 25.00M;
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+        public string CakeWriting { get; set; }
+
+        public BirthdayParty (int numberOfPeople, bool fancyDecorations, string cakeWriting)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            CakeWriting = cakeWriting;
+        }
+
+        private int actualLength
+        {
+            get
+            {
+                if (CakeWriting.Length > maxWritingLength())
+                    return maxWritingLength();
+                else
+                    return CakeWriting.Length;
+            }
+        }
+
+        private int cakeSize()
+        {
+            if (NumberOfPeople <= 4)
+                return 8;
+            else
+                return 16;
+        }
+
+        private int maxWritingLength()
+        {
+            if (cakeSize() == 8)
+                return 16;
+            else
+                return 40;
+        }
+
+        public bool CakeWritingTooLong
+        { 
+            get
+            {
+                if (CakeWriting.Length > maxWritingLength())
+                    return true;
+                else
+                    return false;
+            }       
+        }
+
+        private decimal calculateCostOfDecorations()
+        {
+            decimal costOfDecorations;
+            if (FancyDecorations)
+                costOfDecorations = (NumberOfPeople * 15.00M) + 50.00M;
+            else
+                costOfDecorations = (NumberOfPeople * 7.50M) + 30.00M;
+            return costOfDecorations;
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = calculateCostOfDecorations();
+                totalCost += costOfFoodPerPerson * NumberOfPeople;
+                decimal cakeCost;
+                if (cakeSize() == 8)
+                    cakeCost = 40.00M + actualLength * .25M;
+                else
+                    cakeCost = 75.00M + actualLength * .25M;
+                return totalCost + cakeCost;
+            }
+
+        }
+    }
+}
---
+++ HeadFirst_Csharp/BirthdayParty/BirthdayParty/DinnerParty.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayParty
+{
+    class DinnerParty
+    {
+        public const decimal costOfFoodPerPerson = 25M;
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+        public bool HealthyOption { get; set; }
+
+        public DinnerParty(int numberOfPeople, bool healthyOption, bool fancyDecorations)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            HealthyOption = healthyOption;
+        }
+
+        private decimal calculateCostOfDecorations()
+        {
+            decimal costOfDecorations;
+            if (FancyDecorations)
+            {
+                costOfDecorations = 50M + (NumberOfPeople * 15M);
+            }
+            else
+            {
+                costOfDecorations = 30M + (NumberOfPeople * 7.5M);
+            }
+            return costOfDecorations;
+
+        }
+        private decimal calculateCostOfBeveragesPerPerson()
+        {
+            decimal costOfBeveragePerPerson;
+            if (HealthyOption)
+            {
+                costOfBeveragePerPerson = 5M;
+            }
+            else
+            {
+                costOfBeveragePerPerson = 20M;
+            }
+            return costOfBeveragePerPerson;
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = calculateCostOfDecorations() + ((costOfFoodPerPerson + calculateCostOfBeveragesPerPerson()) * NumberOfPeople);
+                if (HealthyOption)
+                {
+                    totalCost = totalCost * .95M;
+                }
+                return totalCost;
+            }
+        }
+    }
+}
