fb815c270d702cb3aec04302da4298cca8f9307e
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-15 13:55:42 +0900

    how implementing interface works ( compile )

---
+++ HeadFirst_Csharp/IStingPatrol/IStingPatrol/Bee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IStingPatrol
+{
+    class Bee : IStingPatrol
+    {
+        public int AlertLevel
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int StingerLength
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool LookForEnemies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SharpenStinger(int length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
