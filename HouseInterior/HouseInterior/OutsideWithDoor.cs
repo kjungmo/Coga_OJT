2cdce534e7085c7c2a0c96a0d598fec12c6ac65d
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-16 23:30:27 +0900

    House Interior exercise

---
+++ HouseInterior/HouseInterior/OutsideWithDoor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseInterior
+{
+    class OutsideWithDoor : Outside, IHasExteriorDoor
+    {
+        // DoorLocation property
+        // read-only DoorDescription
+    }
+}
---
+++ HouseInterior/HouseInterior/RoomWithDoor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseInterior
+{
+    class RoomWithDoor : Room, IHasExteriorDoor
+    {
+        // DoorLocation property
+        // read-only DoorDescription
+    }
+}
