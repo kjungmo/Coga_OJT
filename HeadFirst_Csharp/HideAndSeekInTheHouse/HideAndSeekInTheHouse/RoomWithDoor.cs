21fb2416d46ff3aa68447ae74a1e89a210290315
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-18 02:03:47 +0900

    final hide and seek

---
+++ HeadFirst_Csharp/HideAndSeekInTheHouse/HideAndSeekInTheHouse/RoomWithDoor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideAndSeekInTheHouse
+{
+    class RoomWithDoor : RoomWithHidingPlace, IHasExteriorDoor
+    {
+        public RoomWithDoor(string name, string decoration, string doorDescription, string hidingPlaceName) : base(name, decoration, hidingPlaceName)
+        {
+            DoorDescription = doorDescription;
+        }
+
+        public Location DoorLocation { get; set; }
+        public string DoorDescription { get; private set; }
+
+    }
+}
