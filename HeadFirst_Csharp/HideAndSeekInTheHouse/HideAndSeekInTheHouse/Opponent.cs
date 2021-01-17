84c4114dcac874a55edfeea79a93e75b19c1e82d
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-18 02:03:13 +0900

    final hide and seek

---
+++ HeadFirst_Csharp/HideAndSeekInTheHouse/HideAndSeekInTheHouse/Opponent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideAndSeekInTheHouse
+{
+    class Opponent
+    {
+        private Random random;
+        private Location myLocation;
+        public Opponent(Location startingLocation)
+        {
+            myLocation = startingLocation;
+            random = new Random();
+        }
+
+        public void move()
+        {
+            bool hidden = false;
+            while (!hidden)
+            {
+                if (myLocation is IHasExteriorDoor)
+                {
+                    IHasExteriorDoor locationWithDoor = myLocation as IHasExteriorDoor;
+                    if (random.Next(2) == 1)
+                        myLocation = locationWithDoor.DoorLocation;
+                }
+                int rand = random.Next(myLocation.exits.Length);
+                myLocation = myLocation.exits[rand];
+                if (myLocation is IHidingPlace)
+                    hidden = true;
+            }
+        }
+
+        public bool check( Location locationToCheck )
+        {
+            if (locationToCheck != myLocation)
+                return false;
+            else
+                return true;
+        }
+    }
+}
