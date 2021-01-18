3b2c14fe58beb35746e449ee749f0ce209a9910b
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-18 14:31:31 +0900

    note : enum should not be created in a seperate cs file

---
+++ HeadFirst_Csharp/PokerWithEnum/PokerWithEnum/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerWithEnum
+{
+    class Card
+    {
+        public Suit suits { get; set; }
+        public Value values { get; set; }
+
+        public string Name 
+        { 
+            get
+            {
+                return values.ToString() + " of " + suits.ToString();
+            }
+        }
+
+        public Card(Suit suits, Value values)
+        {
+            this.suits = suits;
+            this.values = values;
+        }
+    }
+
+}
---
+++ HeadFirst_Csharp/PokerWithEnum/PokerWithEnum/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokerWithEnum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Card card = new Card((Suit)random.Next(4), (Value)random.Next(1, 14));
+            MessageBox.Show(card.Name);
+        }
+    }
+}
