d6c65814acf1f4179f44f347f3d23648202f035a
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-19 16:54:45 +0900

    overloading practice

---
+++ HeadFirst_Csharp/OverloadMyMethod/OverloadMyMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMyMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card cardTocheck = new Card(Suit.Clubs, Value.Three);
+            bool doesItMatch = Card.DoesCardMatch(cardTocheck, Suit.Hearts);
+            Console.WriteLine(doesItMatch);
+
+            Console.ReadKey();
+        }
+    }
+}
