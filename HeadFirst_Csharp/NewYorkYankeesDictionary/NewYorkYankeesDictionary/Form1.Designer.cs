3df096f0c1af93407d2cd1a3ae558a8a5d45e4d9
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-19 19:38:26 +0900

    dictionary and NY yankees

---
+++ HeadFirst_Csharp/NewYorkYankeesDictionary/NewYorkYankeesDictionary/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace NewYorkYankeesDictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playerName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.retiredYear = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // number
+            // 
+            this.number.FormattingEnabled = true;
+            this.number.Location = new System.Drawing.Point(63, 13);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(72, 20);
+            this.number.TabIndex = 1;
+            this.number.SelectedIndexChanged += new System.EventHandler(this.number_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "was worn by";
+            // 
+            // playerName
+            // 
+            this.playerName.Location = new System.Drawing.Point(226, 12);
+            this.playerName.Name = "playerName";
+            this.playerName.ReadOnly = true;
+            this.playerName.Size = new System.Drawing.Size(100, 21);
+            this.playerName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(333, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "and retired in ";
+            // 
+            // retiredYear
+            // 
+            this.retiredYear.Location = new System.Drawing.Point(422, 13);
+            this.retiredYear.Name = "retiredYear";
+            this.retiredYear.ReadOnly = true;
+            this.retiredYear.Size = new System.Drawing.Size(74, 21);
+            this.retiredYear.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 44);
+            this.Controls.Add(this.retiredYear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.playerName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Retired Jersey Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox number;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox playerName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox retiredYear;
+    }
+}
+
