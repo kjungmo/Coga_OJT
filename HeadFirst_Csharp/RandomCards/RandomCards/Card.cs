936d5a4817492fd5980ad4f323791509a9d23a67
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-19 15:33:19 +0900

    random card comparer

---
+++ HeadFirst_Csharp/RandomCards/RandomCards/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCards
+{
+    class Card
+    {
+        public Suit Suits { get; set; }
+        public Value Values { get; set; }
+
+        public string Name 
+        { 
+            get
+            {
+                return Values.ToString() + " of " + Suits.ToString();
+            }
+        }
+
+        public Card(Suit suits, Value values)
+        {
+            this.Suits = suits;
+            this.Values = values;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+}
---
+++ HeadFirst_Csharp/RandomCards/RandomCards/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Five random Cards : ");
+            List<Card> cards = new List<Card>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                cards.Add(new Card((Suit)random.Next(4), (Value)random.Next(1, 14)));
+                Console.WriteLine(cards[i].Name);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Those same Cards, sorted :");
+            cards.Sort(new CardComparerByValue());
+            foreach (Card card in cards)
+            {
+                Console.WriteLine(card.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
