ef7cab60eb241827c22d585c60124642956263b2
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-19 16:37:33 +0900

    IEnumerable and covariance

---
+++ HeadFirst_Csharp/DucksBirdsPenguins/DucksBirdsPenguins/Duck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DucksBirdsPenguins
+{
+    class Duck : Bird, IComparable<Duck>
+    {
+        public int size;
+        public KindOfDuck kind;
+
+        public int CompareTo(Duck duckToCompare)
+        {
+            if (this.size > duckToCompare.size)
+                return 1;
+            else if (this.size < duckToCompare.size)
+                return -1;
+            else 
+                return 0;
+        }
+
+        public override string ToString()
+        {
+            return "A " + size + " inch " + kind.ToString();
+        }
+    }
+}
---
+++ HeadFirst_Csharp/DucksBirdsPenguins/DucksBirdsPenguins/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DucksBirdsPenguins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Duck> ducks = new List<Duck>() 
+            {   new Duck() { kind = KindOfDuck.Mallard, size = 17 },
+                new Duck() { kind = KindOfDuck.Muscovy, size = 18 },
+                new Duck() { kind = KindOfDuck.Decoy, size = 14 },
+                new Duck() { kind = KindOfDuck.Muscovy, size = 11 },
+                new Duck() { kind = KindOfDuck.Mallard, size = 14 },
+                new Duck() { kind = KindOfDuck.Decoy, size = 13 },
+            };
+            IEnumerable<Bird> upcastDucks = ducks;
+
+            List<Bird> birds = new List<Bird>();
+            birds.Add(new Bird() { Name = "Feathers" });
+            birds.AddRange(upcastDucks);
+            birds.Add(new Penguin() { Name = "George" });
+
+            foreach (Bird bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
