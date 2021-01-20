889da038a8dbc2763311d0e3d4aeed43881a7cb5
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-20 16:59:34 +0900

    macow.txt using streamwrite class

---
+++ HeadFirst_Csharp/MacawFlobbo/MacawFlobbo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MacawFlobbo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flobbo f = new Flobbo("blue Yellow");
+            StreamWriter sw = f.Snobbo();
+            f.Blobbo(f.Blobbo(f.Blobbo(sw), sw), sw);
+        }
+    }
+}
