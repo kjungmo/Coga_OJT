a4efb085b7c70df17f8c1149e5121149e5d21f6b
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-01-24 23:24:31 +0900

    added method to change index, when a node is added in between existing nodes

--- DataStructure/DataStructure/LinkedList.cs
+++ DataStructure/DataStructure/LinkedList.cs
@@ -8,12 +8,12 @@ namespace DataStructure
 {
     public class LinkedList
     {
-        //public Node MyLinkedListNode;
+        public Node MyLinkedListNode;
 
-        //public LinkedList()
-        //{
-        //    MyLinkedListNode = null;
-        //}
+        public LinkedList()
+        {
+            MyLinkedListNode = null;
+        }
 
         //public void CreateLinkedNode(string input)
         //{
@@ -25,13 +25,45 @@ namespace DataStructure
 
         public List<Node> MyLinkedList = new List<Node>();
 
+        public List<Node> ChangeIndex(List<Node> linkedlist, int index, string input)
+        {
+            List<Node> leftList = new List<Node>();
+            List<Node> rightList = new List<Node>();
+            List<Node> newMyLinkedList = new List<Node>();
+
+            Node indexNode = new Node(index, input);
+
+            for (int i = 0; i < index; i++)
+            {
+                leftList.Add(MyLinkedList[i]);
+            }
+
+            leftList.Add(indexNode);
+
+            for (int i = index + 1; i < MyLinkedList.Count; i++)
+            {
+                rightList.Add(MyLinkedList[i]);
+            }
+
+            return MergeLists(leftList, rightList);
+        }
+
+        public List<Node> MergeLists(List<Node> left, List<Node> right)
+        {
+            for (int i = 0; i < right.Count; i++)
+            {
+                left.Add(right[i]);
+            }
+            return left;
+        }
+
         public List<Node> CreateLinkedList(int index, string input)
         {
             if (index - 1 >= 0)
             {
                 if (MyLinkedList == null)
                 {
-                    Node head = new Node();
+                    Node head = new Node(input);
                     MyLinkedList.Add(head); // mylinkedlist[0] is head node
                     return MyLinkedList;
                 }
