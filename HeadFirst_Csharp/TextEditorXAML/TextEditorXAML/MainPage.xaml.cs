c794d06a2dea22ada1a620d1dfb8a0439c11d507
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-15 15:52:39 +0900

    added methods, event handlers to code-behind

--- HeadFirst_Csharp/TextEditorXAML/TextEditorXAML/MainPage.xaml.cs
+++ HeadFirst_Csharp/TextEditorXAML/TextEditorXAML/MainPage.xaml.cs
@@ -13,6 +13,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.System;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
+
 
 // 기본 페이지 항목 템플릿에 대한 설명은 http://go.microsoft.com/fwlink/?LinkId=234237에 나와 있습니다.
 
@@ -103,19 +108,91 @@ namespace TextEditorXAML
 
         #endregion
 
+        bool textChanged = false;
+        bool loading = false;
+        IStorageFile saveFile = null;
+
         private void text_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if (loading)
+            {
+                loading = false;
+                return;
+            }
+            if (!textChanged)
+            {
+                filename.Text += "*";
+                saveButton.IsEnabled = true;
+                textChanged = true;
+            }
         }
 
-        private void openButton_Click(object sender, RoutedEventArgs e)
+        private async void openButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (textChanged)
+            {
+                MessageDialog overwriteDialog = new MessageDialog("You have unsaved changes. Are you sure you want to load a new file?");
+                overwriteDialog.Commands.Add(new UICommand("Yes"));
+                overwriteDialog.Commands.Add(new UICommand("No"));
+                overwriteDialog.DefaultCommandIndex = 1;
+                UICommand result = await overwriteDialog.ShowAsync() as UICommand;
+                if (result != null && result.Label == "No")
+                {
+                    return;
+                }
+            }
+            OpenFile();
         }
+
+        private async void OpenFile()
+        {
+            FileOpenPicker picker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.List,
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+            };
+            picker.FileTypeFilter.Add(".txt");
+            picker.FileTypeFilter.Add(".xml");
+            picker.FileTypeFilter.Add(".xaml");
+            IStorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                string fileContents = await FileIO.ReadTextAsync(file);
+                loading = true;
+                text.Text = fileContents;
+                textChanged = false;
+                filename.Text = file.Name;
+                saveFile = file;
+            }
+        }
+
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
+            SaveFile();
+        }
 
+        private async void SaveFile()
+        {
+            if (saveFile != null)
+            {
+                FileSavePicker picker = new FileSavePicker
+                {
+                    DefaultFileExtension = ".txt",
+                    SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+                };
+                picker.FileTypeChoices.Add("Text File", new List<string>() { ".txt" });
+                picker.FileTypeChoices.Add("XML File", new List<string>() { ".xml", ".xaml" });
+                saveFile = await picker.PickSaveFileAsync();
+                if (saveFile == null)
+                {
+                    return;
+                }
+            }
+            await FileIO.WriteTextAsync(saveFile, text.Text);
+            await new MessageDialog("Wrote " + saveFile.Name).ShowAsync();
+            textChanged = false;
+            filename.Text = saveFile.Name;
         }
     }
 }
