74afbef214157fa88d8a05354aba3bb5518bf430
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-15 13:11:21 +0900

    Porting GoFish to XAML project, loaded .cs files

---
+++ HeadFirst_Csharp/AgainGoFish/AgainGoFish/Card.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgainGoFish
+{
+    class Card
+    {
+        public Suit Suits { get; set; }
+        public Value Values { get; set; }
+
+        public Card(Suit suits, Value values)
+        {
+            this.Suits = suits;
+            this.Values = values;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return Values.ToString() + " of " + Suits.ToString();
+            }
+        }
+        
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Suit suit)
+        {
+            if (cardToCheck.Suits == suit)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool DoesCardMatch(Card cardToCheck, Value value)
+        {
+            if (cardToCheck.Values == value)
+                return true;
+            else
+                return false;
+        }
+
+
+        public static string Plural(Value value)
+        {
+            if (value == Value.Six)
+                return "Sixes";
+            else
+                return value.ToString() + "s";
+        }
+    }
+}
