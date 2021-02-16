8435c489d69db55f55ff2567dbdc3dc36ff37453
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-16 17:31:42 +0900

    created struct Dog

---
+++ HeadFirst_Csharp/StructDog/StructDog/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
