8b986c85845364f9ab185dfd14cc28a79bd60616
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-16 17:35:31 +0900

    created Canine class, and added main() methods

--- HeadFirst_Csharp/StructDog/StructDog/Program.cs
+++ HeadFirst_Csharp/StructDog/StructDog/Program.cs
@@ -10,6 +10,19 @@ namespace StructDog
     {
         static void Main(string[] args)
         {
+            Canine spot = new Canine("Spot", "pug");
+            Canine bob = spot;
+            bob.Name = "Spike";
+            bob.Breed = "beagle";
+            spot.Speak();
+
+            Dog jake = new Dog("Jake", "poodle");
+            Dog betty = jake;
+            betty.Name = "Betty";
+            betty.Breed = "pit bull";
+            jake.Speak();
+
+            Console.ReadKey();
         }
     }
 }
