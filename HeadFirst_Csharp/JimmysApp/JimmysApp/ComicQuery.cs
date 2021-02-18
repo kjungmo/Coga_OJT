1d7d5b07e1ea28dc837874befceba5401584f0f5
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-18 15:12:14 +0900

    added Comic class, ComicQuery class

---
+++ HeadFirst_Csharp/JimmysApp/JimmysApp/ComicQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JimmysApp
+{
+    class ComicQuery
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+
+        public ComicQuery(string title, string subtitle, string description, string imagePath)
+        {
+            Title = title;
+            Subtitle = subtitle;
+            Description = description;
+            ImagePath = imagePath;
+        }
+    }
+}
