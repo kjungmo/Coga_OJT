f9028e12283df89fd1756dbdcbcc8500c8fbfdcc
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-18 16:23:23 +0900

    created ComicQuery class

--- HeadFirst_Csharp/JimmysApp/JimmysApp/ComicQuery.cs
+++ HeadFirst_Csharp/JimmysApp/JimmysApp/ComicQuery.cs
@@ -8,10 +8,10 @@ namespace JimmysApp
 {
     class ComicQuery
     {
-        public string Title { get; set; }
-        public string Subtitle { get; set; }
-        public string Description { get; set; }
-        public string ImagePath { get; set; }
+        public string Title { get; private set; }
+        public string Subtitle { get; private set; }
+        public string Description { get; private set; }
+        public string ImagePath { get; private set; }
 
         public ComicQuery(string title, string subtitle, string description, string imagePath)
         {
