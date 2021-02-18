97dd264eca37a26e5e44bf2dfc37199273e0e255
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-18 12:35:09 +0900

    extension method

---
+++ HeadFirst_Csharp/ExtendingBasicType/ExtendingBasicType/AnyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyExtension;
+
+
+namespace ExtendingBasicType
+{
+    class AnyClass 
+    {
+        string message = "Clones are wreaking havoc at the factory. Help!";
+        
+    }
+}
