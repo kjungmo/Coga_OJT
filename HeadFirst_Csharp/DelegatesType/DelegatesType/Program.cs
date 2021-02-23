8cb95b24ce510b38780d40397c8d2fd47af9a0d6
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-23 10:48:04 +0900

    added ConvertsIntToString type variable to Main method

--- HeadFirst_Csharp/DelegatesType/DelegatesType/Program.cs
+++ HeadFirst_Csharp/DelegatesType/DelegatesType/Program.cs
@@ -8,8 +8,16 @@ namespace DelegatesType
 {
     class Program
     {
+        private static string HiThere(int i)
+        {
+            return "Hi there! #" + (i * 100);
+        }
         static void Main(string[] args)
         {
+            ConvertsIntToString someMethod = new ConvertsIntToString(HiThere);
+            string message = someMethod(5);
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
