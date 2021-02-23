17e8a16ebad384f2c61251387201c433888d7a4b
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-23 10:45:56 +0900

    created class but used delegate

---
+++ HeadFirst_Csharp/DelegatesType/DelegatesType/ConvertsIntToString.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesType
+{
+    delegate string ConvertsIntToString(int i);
+}
