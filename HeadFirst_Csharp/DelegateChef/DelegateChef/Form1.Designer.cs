a18f368f5c860b9d74383a63c591bf5c3e809dc4
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-23 11:11:57 +0900

    created Form

--- HeadFirst_Csharp/DelegateChef/DelegateChef/Form1.Designer.cs
+++ HeadFirst_Csharp/DelegateChef/DelegateChef/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.useIngredient = new System.Windows.Forms.Button();
+            this.getSuzanne = new System.Windows.Forms.Button();
+            this.getAmy = new System.Windows.Forms.Button();
+            this.amount = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.amount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // useIngredient
+            // 
+            this.useIngredient.Location = new System.Drawing.Point(38, 29);
+            this.useIngredient.Name = "useIngredient";
+            this.useIngredient.Size = new System.Drawing.Size(168, 23);
+            this.useIngredient.TabIndex = 0;
+            this.useIngredient.Text = "Get the ingredient";
+            this.useIngredient.UseVisualStyleBackColor = true;
+            this.useIngredient.Click += new System.EventHandler(this.useIngredient_Click);
+            // 
+            // getSuzanne
+            // 
+            this.getSuzanne.Location = new System.Drawing.Point(38, 79);
+            this.getSuzanne.Name = "getSuzanne";
+            this.getSuzanne.Size = new System.Drawing.Size(218, 23);
+            this.getSuzanne.TabIndex = 1;
+            this.getSuzanne.Text = "Get Suzanne\'s delegate";
+            this.getSuzanne.UseVisualStyleBackColor = true;
+            this.getSuzanne.Click += new System.EventHandler(this.getSuzanne_Click);
+            // 
+            // getAmy
+            // 
+            this.getAmy.Location = new System.Drawing.Point(38, 131);
+            this.getAmy.Name = "getAmy";
+            this.getAmy.Size = new System.Drawing.Size(218, 23);
+            this.getAmy.TabIndex = 2;
+            this.getAmy.Text = "Get Amy\'s delegate";
+            this.getAmy.UseVisualStyleBackColor = true;
+            this.getAmy.Click += new System.EventHandler(this.getAmy_Click);
+            // 
+            // amount
+            // 
+            this.amount.Location = new System.Drawing.Point(212, 30);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(44, 21);
+            this.amount.TabIndex = 3;
+            this.amount.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 187);
+            this.Controls.Add(this.amount);
+            this.Controls.Add(this.getAmy);
+            this.Controls.Add(this.getSuzanne);
+            this.Controls.Add(this.useIngredient);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Secret Ingredients";
+            ((System.ComponentModel.ISupportInitialize)(this.amount)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button useIngredient;
+        private System.Windows.Forms.Button getSuzanne;
+        private System.Windows.Forms.Button getAmy;
+        private System.Windows.Forms.NumericUpDown amount;
     }
 }
 
--- HeadFirst_Csharp/DelegateChef/DelegateChef/Form1.cs
+++ HeadFirst_Csharp/DelegateChef/DelegateChef/Form1.cs
@@ -16,5 +16,29 @@ namespace DelegateChef
         {
             InitializeComponent();
         }
+
+        GetSecretIngredient ingredientMethod = null;
+        Suzanne suzanne = new Suzanne();
+        Amy amy = new Amy();
+
+        private void useIngredient_Click(object sender, EventArgs e)
+        {
+            if (ingredientMethod != null)
+            {
+                Console.WriteLine("I'll add " + ingredientMethod((int)amount.Value));
+            }
+        }
+
+        private void getSuzanne_Click(object sender, EventArgs e)
+        {
+            ingredientMethod = new GetSecretIngredient(suzanne.MySecretIngredientMethod);
+            // ingredientMethod = suzanne.MySecretIngredientMethod; (implicit type conversion)
+        }
+
+        private void getAmy_Click(object sender, EventArgs e)
+        {
+            ingredientMethod = new GetSecretIngredient(amy.AmysSecretIngredientMethod);
+            // ingredientMethod = amy.AmysSecretIngredientMethod; (implicit type conversion)
+        }
     }
 }
