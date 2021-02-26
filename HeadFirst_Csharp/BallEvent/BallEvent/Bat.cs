18ebd0f7e7254fda2b8060c0b14dd3909cce9ab9
kjungmo <63198352+kjungmo@users.noreply.github.com>
2021-02-26 10:43:43 +0900

    added Bat class

---
+++ HeadFirst_Csharp/BallEvent/BallEvent/Bat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallEvent
+{
+    class Bat
+    {
+        private BatCallback hitBallCallback;
+        public Bat(BatCallback callbackDelegate)
+        {
+            this.hitBallCallback = new BatCallback(callbackDelegate);
+        }
+        public void HitTheBall(BallEventArgs e)
+        {
+            if (hitBallCallback != null)
+            {
+                hitBallCallback(e);
+            }
+        }
+    }
+}
